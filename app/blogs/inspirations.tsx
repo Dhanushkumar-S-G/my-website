b4789fb4e3e923084ed8ecce6eef0663d9514dfa
Dhanushkumar-S-G <dhanushkumarganapathy@gmail.com>
2026-01-31 09:02:30 +0000

    added insipirations

---
+++ app/blogs/inspirations.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { CardStack } from "@/components/ui/card-stack";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { ExternalLink } from 'lucide-react';
+export function Inspirations() {
+  return (
+    <div className="h-auto flex items-center justify-center w-full">
+      <CardStack items={CARDS} />
+    </div>
+  );
+}
+
+// Small utility to highlight the content of specific section of a testimonial content
+export const Highlight = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <span
+      className={cn(
+        "font-medium rounded-sm px-1 py-0.5 \
+         bg-[#9441F5]/18 text-[#7A3FEA] \
+         dark:bg-[#9441F5]/25 dark:text-[#C9A8FF]",
+        className
+      )}
+    >
+      {children}
+    </span>
+  );
+};
+
+
+
+const CARDS = [
+  {
+    id: 0,
+    name: <div className="flex items-center gap-2">
+          <Link
+            href="https://x.com/akshaymarch7"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-1 cursor-pointer hover:underline"
+          >
+            Akshay Saini
+            <ExternalLink size={16} />
+          </Link>
+        </div>,
+    designation: "Software Engineer",
+    content: (
+      <p>
+        Want to truly learn? 
+        Write about what you know. 
+
+        <Highlight className="">You’ll realize how much you don’t.</Highlight>
+      </p>
+    ),
+  }
+];
--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,4 +1,5 @@
 import { BlogCard } from "@/components/blog-card";
+import {Inspirations} from "./inspirations";
 
 
 
@@ -6,6 +7,7 @@ export default function Blogs() {
   return (
     <div className="mx-auto max-w-3xl py-30 prose prose-invert">
       <div className="grid grid-cols-1 gap-5">
+        < Inspirations />
         <BlogCard
           title="Building Modern UIs: A Deep Dive into Shadcn and React Components"
           description="Join us for an in-depth exploration of building modern user interfaces using shadcn/ui and React. Learn best practices and advanced techniques."
